a85950d4b74780b5b77677dbfc8935b68ca1fb3b
Mher Keshishian <Mkeshishian7933@conestogac.on.ca>
2024-03-23 15:37:38 -0400

    Implemented like/unlike functionality

--- src/posts/views.py
+++ src/posts/views.py
@@ -3,7 +3,6 @@ from .models import Post
 from django.http import JsonResponse
 
 # Create your views here.
-
 def post_list_and_create(request):
     qs = Post.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
@@ -27,6 +26,18 @@ def load_post_data_view(request, num_posts):
         }
         data.append(item)
     return JsonResponse({'data':data[lower:upper], 'size': size})
+
+def like_unlike_post(request):
+    if request.is_ajax():
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world'})
