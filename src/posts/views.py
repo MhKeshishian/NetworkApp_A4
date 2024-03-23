076ae077f80e5f2724f048751ec96e6fa792c91a
Mher Keshishian <Mkeshishian7933@conestogac.on.ca>
2024-03-23 15:49:34 -0400

    Improved AJAX handling and security in views

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Create your views here.
 def post_list_and_create(request):
@@ -20,24 +21,44 @@ def load_post_data_view(request, num_posts):
             'id': obj.id,
             'title': obj.title,
             'body': obj.body,
-            'liked': True if request.user in obj.liked.all() else False,
+            #'liked': True if request.user in obj.liked.all() else False,
+            'liked': True if request.user.is_authenticated and request.user in obj.liked.all() else False,  # Check if the user is authenticated
             'count': obj.like_count,
             'author':obj.author.user.username
         }
         data.append(item)
     return JsonResponse({'data':data[lower:upper], 'size': size})
 
+# def like_unlike_post(request):
+#     if request.is_ajax():
+#         pk = request.POST.get('pk')
+#         obj = Post.objects.get(pk=pk)
+#         if request.user in obj.liked.all():
+#             liked = False
+#             obj.liked.remove(request.user)
+#         else:
+#             liked = True
+#             obj.liked.add(request.user)
+#         return JsonResponse({'liked': liked, 'count': obj.like_count})
+
+@ensure_csrf_cookie  # Ensure that CSRF cookie is set
 def like_unlike_post(request):
-    if request.is_ajax():
+    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':  # Check if it's a POST request and AJAX
         pk = request.POST.get('pk')
         obj = Post.objects.get(pk=pk)
-        if request.user in obj.liked.all():
-            liked = False
-            obj.liked.remove(request.user)
+        if request.user.is_authenticated:
+            if request.user in obj.liked.all():
+                liked = False
+                obj.liked.remove(request.user)
+            else:
+                liked = True
+                obj.liked.add(request.user)
+            return JsonResponse({'liked': liked, 'count': obj.like_count})
         else:
-            liked = True
-            obj.liked.add(request.user)
-        return JsonResponse({'liked': liked, 'count': obj.like_count})
+            return JsonResponse({'error': 'User not authenticated'}, status=403)  # Return error if user is not authenticated
+    else:
+        return JsonResponse({'error': 'Invalid request'}, status=400)  # Return error for invalid request
+
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world'})
