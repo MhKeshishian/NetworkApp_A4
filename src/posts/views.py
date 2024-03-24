2b704f15b939a4d17730f160a1bf4855cdd87f80
Mher Keshishian <Mkeshishian7933@conestogac.on.ca>
2024-03-24 12:59:30 -0400

    Added Dropzone

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Post
-from django.http import JsonResponse
+from .models import Post, Photo
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from .forms import PostForm
 from profiles.models import Profile
@@ -120,3 +120,12 @@ def delete_post(request, pk):
     if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
         obj.delete()
         return JsonResponse({})
+    
+def image_upload_view(request):
+    # print(request.FILES)
+    if request.method == 'POST':
+        img = request.FILES.get('file')
+        new_post_id = request.POST.get('new_post_id')
+        post = Post.objects.get(id=new_post_id)
+        Photo.objects.create(image=img, post=post)
+    return HttpResponse()
